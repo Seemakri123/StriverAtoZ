ca8429ffeaf3d48ede2c038ebd648d0c2649ba3d
Seema Kumari <seema.kri870@gmail.com>
2025-05-07 22:35:00 +0530

    added a code

---
+++ Step 1/Lec 1/1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Solution{
+    public:
+    void printNumber(){
+        int num;
+        cin>>num;
+        cout<<num;
+    }
+};
+
+int main(){
+    Solution obj;
+    obj.printNumber();
+    return 0;
+}
