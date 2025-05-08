f141bd6e84c66f2fdeeea10f3d0204741857ccd2
Seema Kumari <seema.kri870@gmail.com>
2025-05-08 08:59:14 +0530

    added more codes

--- Step 1 - Basic/Lec 2/4.cpp
+++ Step 1 - Basic/Lec 2/4.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main(){
     int n;
-    cin>> n;
-    for(int i =0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<"*";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
         }cout<<endl;
     }
     return 0;
