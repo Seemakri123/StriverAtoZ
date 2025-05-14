c60536ac6a2f0f340760914ef6acfbea3023f109
Seema Kumari <seema.kri870@gmail.com>
2025-05-14 17:23:38 +0530

    edit a code

--- Step 1 - Basic/Lec 2/11.cpp
+++ Step 1 - Basic/Lec 2/11.cpp
@@ -4,9 +4,11 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<=i;j++){
-            cout << ((i + j) % 2) << " ";
+    for(int i=1;i<=n;++i){
+        int val = i%2;
+        for(int j=1;j<=i;++j){
+            cout << val << " ";
+            val=1-val;
         }cout<<endl;
     }
     return 0;
